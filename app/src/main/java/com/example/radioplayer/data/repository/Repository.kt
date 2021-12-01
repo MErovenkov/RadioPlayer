b91ddec6270ee362ff380e50154d6affc2260a4c
MErovenkov <matvey.erovenkov96@gmail.com>
2021-12-01 22:04:31 +0300

    Synchronization work with radioList

--- app/src/main/java/com/example/radioplayer/data/repository/Repository.kt
+++ app/src/main/java/com/example/radioplayer/data/repository/Repository.kt
@@ -1,21 +1,38 @@
 package com.example.radioplayer.data.repository
 
 import com.google.android.exoplayer2.MediaItem
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.io.FileNotFoundException
 
 class Repository(private val radioLocalData: RadioLocalData) {
 
-    private var radioList: List<MediaItem> = listOf()
+    private val mutex = Mutex()
+    private var radioList: List<MediaItem> = emptyList()
 
     @Throws(FileNotFoundException::class)
     fun getRadioItemList(): Flow<List<MediaItem>> = radioLocalData.getRadioItemList()
         .map {
-            radioList = it
-            radioList
+            mutex.withLock(radioList) {
+                radioList = it
+                radioList
+            }
         }
+        .flowOn(Dispatchers.IO)
 
+    @Suppress("BlockingMethodInNonBlockingContext")
     fun getRadioItem(title: String): Flow<MediaItem?> = flow {
-        emit(radioList.first{ mediaItem -> mediaItem.mediaMetadata.title == title})
-    }
+            if (radioList.isEmpty()) {
+                getRadioItemList().collect()
+            }
+
+            val radioItem: MediaItem? = mutex.withLock(radioList) {
+                radioList.firstOrNull { mediaItem -> mediaItem.mediaMetadata.title == title }
+            }
+
+            emit(radioItem)
+        }
+        .flowOn(Dispatchers.IO)
 }
